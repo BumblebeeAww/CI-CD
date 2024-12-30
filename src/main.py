947a48f8c4fcb18185e60b706754c940d62576b5
BumblebeeAww <Therainbowlove20@gmail.com>
2024-12-30 11:56:08 +0300

    Обновление конфигурации GitHub Actions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-def add(a, b):
-    return a + b
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Определяем маршрут для сложения двух чисел
+@app.route('/add', methods=['GET'])
+def add():
+    # Получаем параметры a и b из URL
+    a = request.args.get('a', type=int)
+    b = request.args.get('b', type=int)
+    
+    # Возвращаем результат сложения в формате JSON
+    result = a + b
+    return jsonify(result=result)
 
 if __name__ == "__main__":
-    print(add(2, 3))
+    app.run(debug=True)
